c33cb78462bfc9402c6a37a7a8cdf9b27ffa0fc0
sirpuffin1 <angelcruz981@yahoo.com>
2021-10-03 12:55:08 -0700

    chore(added some comments, used prettier)

--- ngxs-intro/src/app/state/tutorial.state.ts
+++ ngxs-intro/src/app/state/tutorial.state.ts
@@ -1,37 +1,41 @@
-import { State, Action, StateContext, Selector } from "@ngxs/store";
-import { Tutorial } from "../models/tutorial.model";
-import { AddTutorial, RemoveTutorial } from "../actions/tutorial.action";
-
+import { State, Action, StateContext, Selector } from '@ngxs/store';
+import { Tutorial } from '../models/tutorial.model';
+import { AddTutorial, RemoveTutorial } from '../actions/tutorial.action';
+//create a state model
 export class TutorialStateModel {
   tutorials: Tutorial[] = [];
 }
-
+//use state decorator to define a name and default values based on the state model above
 @State<TutorialStateModel>({
   name: 'tutorials',
   defaults: {
-      tutorials: []
-  }
+    tutorials: [],
+  },
 })
-
 export class TutorialState {
-
   @Selector()
   static getTutorials(state: TutorialStateModel) {
-    return state.tutorials
+    return state.tutorials;
   }
 
   @Action(AddTutorial)
-  add({getState, patchState }: StateContext<TutorialStateModel>, { payload }: AddTutorial) {
+  add(
+    { getState, patchState }: StateContext<TutorialStateModel>,
+    { payload }: AddTutorial
+  ) {
     const state = getState();
     patchState({
-      tutorials: [...state.tutorials, payload]
-    })
+      tutorials: [...state.tutorials, payload],
+    });
   }
 
   @Action(RemoveTutorial)
-  remove({getState, patchState }: StateContext<TutorialStateModel>, { payload }: RemoveTutorial) {
+  remove(
+    { getState, patchState }: StateContext<TutorialStateModel>,
+    { payload }: RemoveTutorial
+  ) {
     patchState({
-      tutorials: getState().tutorials.filter(a => a.name != payload)
-    })
+      tutorials: getState().tutorials.filter((a) => a.name != payload),
+    });
   }
 }
